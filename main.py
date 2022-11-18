ff01df9bf9981463590de7e78ae47c472c75a8b4
rich <richard.lorenz@gmail.com>
2022-11-18 14:21:39 +1100

    Working background overwrite

--- main.py
+++ main.py
@@ -49,13 +49,13 @@ class Mower:
         self.centre = (MOWER_DIAMETER / 2, MOWER_DIAMETER / 2)
         self.angle_in_degrees = 0
         self.wheel_angle_in_degrees = -10
-        self.base = pg.display.set_mode([MOWER_DIAMETER, MOWER_DIAMETER], pg.SRCALPHA, 32)
+        self.base = pg.Surface([MOWER_DIAMETER, MOWER_DIAMETER], pg.SRCALPHA, 32)
         self.build_base()
         self.image = self.base
 
     def build_base(self):
-        mower_base = pg.display.set_mode([MOWER_DIAMETER, MOWER_DIAMETER], pg.SRCALPHA, 32)
-        mower_base = mower_base.convert_alpha()
+        mower_base = pg.Surface([MOWER_DIAMETER, MOWER_DIAMETER], pg.SRCALPHA, 32)
+#        mower_base = mower_base.convert_alpha()
         # Draw mower body/blade on the mower
         pg.draw.circle(mower_base, (255, 0, 0), (MOWER_DIAMETER / 2, MOWER_DIAMETER / 2), MOWER_DIAMETER / 2)
         # Draw right back wheel on the mower
@@ -68,13 +68,12 @@ class Mower:
                      (MOWER_DIAMETER - WHEEL_SIZE[0], 0) + WHEEL_SIZE)
         self.base = mower_base
 
-
     def build_image(self):
         # Get the base image
         mower_img = self.base
 
         # Create a rotated front wheel image
-        wheel_img = pg.display.set_mode([WHEEL_SIZE[0] + 2, WHEEL_SIZE[1] + 2], pg.SRCALPHA, 32)
+        wheel_img = pg.Surface([WHEEL_SIZE[0] + 2, WHEEL_SIZE[1] + 2], pg.SRCALPHA, 32)
         wheel_img = wheel_img.convert_alpha()
         pg.draw.rect(wheel_img,
                      WHEEL_COLOUR,
@@ -93,27 +92,30 @@ class Mower:
 class Field:
     def __init__(self):
 
-        self.screen = pg.display.set_mode(FIELD_SIZE)
+        self.grass = pg.Surface(FIELD_SIZE)
+        self.grass.fill(GRASS_UNCUT_COLOUR)
 
     def count_colour(self, colour):
         count = 0
         for i in range(FIELD_SIZE[0]):
             for j in range(FIELD_SIZE[1]):
-                if self.screen.get_at((i, j))[0:3] == colour:
+                if self.grass.get_at((i, j))[0:3] == colour:
                     count += 1
         return count
 
-#    def mow(self):
-#        self.screen.blit(mower.base, (300, 300)))
+    def mow(self, this_mower):
+        pg.draw.circle(self.grass, GRASS_CUT_COLOUR, this_mower.centre, MOWER_DIAMETER / 2)
 
 
 pg.init()
 mower = Mower()
 field = Field()
 clock = pg.time.Clock()
+screen = pg.display.set_mode(FIELD_SIZE)
+screen.blit(field.grass, (0, 0))
 
-rect = pg.Rect(0, 0, 20, 20)
-rect.center = field.screen.get_rect().center
+# rect = pg.Rect(0, 0, 20, 20)
+# rect.center = field.grass.get_rect().center
 vel = 5
 
 run = True
@@ -123,28 +125,40 @@ while run:
         if event.type == pg.QUIT:
             run = False
         if event.type == pg.KEYDOWN:
+            screen.blit(field.grass, (0, 0))
             print(pg.key.name(event.key))
 
     keys = pg.key.get_pressed()
 
+    if keys[pg.K_q]:
+        screen.blit(mower.base, (0, 0))
+    if keys[pg.K_w]:
+        screen.blit(mower.image, (0, 0))
+    if keys[pg.K_e]:
+        field.mow(mower)
+    if keys[pg.K_r]:
+        print("r")
+        screen.blit(field.grass, (0, 0))
+    # if keys[pg.K_t]:
+    #     # nothing
     if keys[pg.K_LEFT]:
         print("keys_x", keys[pg.K_x])
-        pg.draw.rect(field.screen, (0, 0, 255), rect)
-        field.screen.blit(mower.base, (200, 200))
+        pg.draw.rect(field.grass, (0, 0, 255), rect)
+        field.grass.blit(mower.base, (200, 200))
     elif keys[pg.K_RIGHT]:
         rect.x += (keys[pg.K_RIGHT] - keys[pg.K_LEFT]) * vel
         rect.y += (keys[pg.K_DOWN] - keys[pg.K_UP]) * vel
 
-        rect.centerx = rect.centerx % field.screen.get_width()
-        rect.centery = rect.centery % field.screen.get_height()
+        rect.centerx = rect.centerx % field.grass.get_width()
+        rect.centery = rect.centery % field.grass.get_height()
 
-        field.screen.fill(0)
-        pg.draw.rect(field.screen, (255, 0, 0), rect)
+        field.grass.fill(0)
+        pg.draw.rect(field.grass, (255, 0, 0), rect)
     elif keys[pg.K_UP]:
-        field.screen.blit(mower.base, (300, 300))
+        field.grass.blit(mower.base, (300, 300))
     elif keys[pg.K_DOWN]:
         mower.build_image()
-        field.screen.blit(mower.image, (400, 400))
+        field.grass.blit(mower.image, (400, 400))
     else:
         pg.display.flip()
 
